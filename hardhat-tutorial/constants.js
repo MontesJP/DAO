dfb4d3cb46fd231c6a49923f23320b09d033272b
MontesJP <juan.pablo.montes99@gmail.com>
2022-05-06 13:10:44 +0200

    Finished DAO project

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+    '0x0ba4c95f1D92452db1008870B0EEd5CEe472775e';
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
